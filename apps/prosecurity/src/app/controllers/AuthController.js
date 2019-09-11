b5face5ed3127dc33c6bc977e7ce11017391140d
jssolucoes-e-servicos <jssolucoeseservicos@gmail.com>
2019-09-11 10:38:25 -0300

    Update AuthController.js

--- apps/prosecurity/src/app/controllers/AuthController.js
+++ apps/prosecurity/src/app/controllers/AuthController.js
@@ -5,7 +5,6 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authConfig = require('./../../config/auth');
-const mailer = require('../../modules/mailer');
 const router = express.Router();
 
 router.post('/customer/register', async (req, res) => {
@@ -76,33 +75,4 @@ router.post('/contributor/authenticate', async (req, res) => {
     return res.send( result );
 });
 
-router.post('/forgot-password', async (req, res) =>{
-    const { Email } = req.body;
-    try {
-        const customer = Customer.findOne( { Email } );
-        if (!customer)
-            res.status(400).send({ error : 'Usuário não encontrado!' }); 
-        const token = crypto.randomBytes(20).toString('hex');
-        const now = new Date();
-        now.setHours(now.getHours() + 1);
-        await Customer.findByIdAndUpdate(customer.id, {
-           '$set': {
-                PasswordResetToken: token,
-                PasswordResetExpires: now
-           } 
-        });
-        let mail = await mailer.sendMail({
-            to: Email,
-            from: 'no-replay@apps.jssolucoeseservicos.com.br',
-            template: 'auth/forgot_password',
-            context: { token }
-        }, (err) => {
-            if (err)
-                return res.status(400).send({ error: 'Cannot send forgot password email'});
-            return res.send();
-        });
-    } catch (err){
-        res.status(400).send({ error : 'Error on forgot password, try again' });
-    }
-});
 module.exports = app => app.use('/prosecurity/auth', router);
